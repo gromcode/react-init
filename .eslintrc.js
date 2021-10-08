78a12e1cd92d52caf7e621099f7920e7236f7b0d
DvijIt <andreydnb@ukr.net>
2021-10-08 14:42:45 +0300

    123

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,12 @@
 module.exports = {
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-  extends: ['airbnb-base', 'prettier'],
-  parser: 'babel-eslint',
-=======
   plugins: ['babel', 'import', 'prettier'],
   extends: ['airbnb-base', 'plugin:react/recommended'],
->>>>>>> Stashed changes
-=======
-  extends: ['eslint-config-airbnb-base', 'prettier', 'plugin:react/recommended'],
-  parser: '@babel/eslint-parser',
->>>>>>> bd960c3b2d407b5a78e2b7d846d35a5f2937aa16
   env: {
     es6: true,
     node: true,
     jest: true,
     browser: true,
   },
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-  rules: {
-=======
   parser: '@babel/eslint-parser',
 
   parserOptions: {
@@ -33,25 +19,6 @@ module.exports = {
   rules: {
     'import/no-unresolved': 0,
     'react/prop-types': 0,
->>>>>>> Stashed changes
-    'no-console': 0,
-    'no-alert': 0,
-    'import/prefer-default-export': 0,
-    'prefer-template': 0,
-=======
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 2021,
-    sourceType: 'module',
-  },
-  rules: {
->>>>>>> bd960c3b2d407b5a78e2b7d846d35a5f2937aa16
-    'import/extensions': 0,
-    'import/no-unresolved': 0,
-    'react/prop-types': 0,
-    'import/prefer-default-export': 0,
     'no-console': 0,
   },
   settings: {
