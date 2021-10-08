dd8f263718f30a3a262e0fc7269256fd49f696b4
DvijIt <andreydnb@ukr.net>
2021-10-08 14:37:18 +0300

    123

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,32 @@
 module.exports = {
+<<<<<<< Updated upstream
   extends: ['airbnb-base', 'prettier'],
   parser: 'babel-eslint',
+=======
+  plugins: ['babel', 'import', 'prettier'],
+  extends: ['airbnb-base', 'plugin:react/recommended'],
+>>>>>>> Stashed changes
   env: {
     es6: true,
     jest: true,
     browser: true,
   },
+<<<<<<< Updated upstream
   rules: {
+=======
+  parser: '@babel/eslint-parser',
+
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+  rules: {
+    'import/no-unresolved': 0,
+    'react/prop-types': 0,
+>>>>>>> Stashed changes
     'no-console': 0,
     'no-alert': 0,
     'import/prefer-default-export': 0,
@@ -19,5 +39,16 @@ module.exports = {
     'no-restricted-globals': 0,
     'func-names': 0,
     'no-underscore-dangle': 0,
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx'],
+        moduleDirectory: ['node_modules', 'src/'],
+      },
+    },
+    react: {
+      version: '16.14',
+    },
   },
 };
--- src/App.jsx
+++ src/App.jsx
@@ -4,4 +4,9 @@ const App = () => {
   return <div>Hello, React!</div>;
 };
 
+<<<<<<< Updated upstream:src/App.jsx
+=======
+// sasas
+
+>>>>>>> Stashed changes:task/src/App.jsx
 export default App;
